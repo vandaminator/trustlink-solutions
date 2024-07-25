0af5d2216defa6506c1b713a488224cdbf369999
vandaminator <ntokoanephomolo76@gmail.com>
2024-07-25 12:31:01 +0200

    Added Seo for twitter and facebook cards

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string;
   readonly SUPABASE_ANON_KEY: string;
+  readonly SITE_NAME: string;
 }
 
 interface ImportMeta {
--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -14,7 +14,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: "http://localhost:4321/api/auth/callback"
+        redirectTo: import.meta.env.SITE_NAME + "/api/auth/callback",
       },
     });
 
@@ -38,7 +38,8 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     return new Response(error.message, { status: 500 });
   }
 
-  const { access_token, refresh_token } = data.session;
+  const { access_token, refresh_token, expires_in } = data.session;
+  const expireTime = Date.now();
   cookies.set("sb-access-token", access_token, {
     path: "/",
   });
